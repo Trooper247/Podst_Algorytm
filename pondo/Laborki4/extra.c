08d60ca073d1d43674b543c25844920b8864112e
Oskar <peronidon247@gmail.com>
2021-10-26 18:39:43 +0200

    bubble sorting algorithm

--- pondo/Laborki4/extra.c
+++ pondo/Laborki4/extra.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 int main()
 {
-
-    int tab[20];
+    int size;
+    int next;
+    printf("Please provide the size of a random array: ");
+    scanf("%d",&size);
+    int tab[size];
+    //size= sizeof tab / sizeof tab[0];
     srand(time(NULL));
-    for(int i=0;i<20;i++){
+    for(int i=0;i<size;i++){
         tab[i] = rand()%10;
     }
-    for(int i =0;i<20;i++){
+    for(int i =0;i<size;i++){
         printf("%d ",tab[i]);
     }
-    int last = tab[0]
-    for(int i=0;i<20;i++){
-        if(tab[i]){}
+    printf("\n");
 
+    int ok = 0;
+    while(ok == 0){
+        for(int i=0;i<size;i++){//organize
+
+            next = tab[(i+1)];
+            if(tab[i]>tab[(i+1)]){
+                tab[(i+1)] = tab[i];
+                tab[i] = next;
+                for(int n =0;n<size;n++){
+                    SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), (tab[n]+2));
+                    printf("%d ",tab[n]);
+                }
+                printf("\n");
+            }
+
+        }
+        ok = 1;
+        for(int x=0;x<size;x++){
+            if(tab[x]>tab[(x+1)]){
+                ok = 0;
+            }
+        }//verify if is in order
     }
+
+
+
     return 0;
 }
 
